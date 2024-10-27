51c8a382f0e4c3c5b75cce09d17cffb9a9c71961
Christian <christian.kartika1@gmail.com>
2024-10-27 16:38:22 +0700

    Initialize Project

---
+++ Union/UnionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnionApp.swift
+//  Union
+//
+//  Created by Christian on 27/10/24.
+//
+
+import SwiftUI
+
+@main
+struct UnionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
